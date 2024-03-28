a6cce873a44bd874ac7ce6e7fdfce8e377877c6f
Josh Glover <25733402+josh-glover@users.noreply.github.com>
2024-03-28 00:27:48 -0500

    Add initial DataContext for database operations

---
+++ src/BargainMagic.Api.Service/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BargainMagic.Api.Service
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var localDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var localApplicationDirectoryPath = Path.Combine(localDirectoryPath, "BargainMagic");
+            var applicationDatabaseDirectoryPath = Path.Combine(localApplicationDirectoryPath, "databases");
+            var applicationDatabaseFilePath = Path.Combine(applicationDatabaseDirectoryPath, "BargainMagic.db");
+
+            optionsBuilder.UseSqlite(applicationDatabaseFilePath);
+        }
+    }
+}
